4d24aac8702c34a373f3a276a42a6f7dbd25d3ad
Negutzul <neaguandrei47@yahoo.com>
2026-02-22 11:55:40 +0200

    Test commit for PR

--- obj/Debug/net10.0/FGP.Server.AssemblyInfo.cs
+++ obj/Debug/net10.0/FGP.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FGP.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c38b9a3ace57ca801c8bad949827412dd94abb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aeae6b40330509c0b0f79f519d1bf95baab761f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FGP.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FGP.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
