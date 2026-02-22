a89af19122bc5ec0162a444b68384d3fe9d702e3
Negutzul <neaguandrei47@yahoo.com>
2026-02-22 20:48:47 +0200

    4th push for test

--- obj/Debug/net10.0/FGP.Server.AssemblyInfo.cs
+++ obj/Debug/net10.0/FGP.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FGP.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aeae6b40330509c0b0f79f519d1bf95baab761f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccdddd65f7203ea7ddf4947ba734e65cb7e59f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("FGP.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FGP.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
